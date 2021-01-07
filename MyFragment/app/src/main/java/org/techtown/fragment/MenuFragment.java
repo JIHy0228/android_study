4fd1f6598793488cf4e74b4530a43f7482710a92
JIHy0228 <vkdnjtjsej07@naver.com>
2021-01-07 17:35:25 +0900

    안드로이드 앱 프로그래밍 챕터2-5프래그먼트

---
+++ MyFragment/app/src/main/java/org/techtown/fragment/MenuFragment.java
@@ -0,0 +1,26 @@
+package org.techtown.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.fragment.app.Fragment;
+
+public class MenuFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup menuview = (ViewGroup) inflater.inflate(R.layout.fragment_menu, container, false);
+        Button button = menuview.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity activity = (MainActivity) getActivity();
+                activity.onFragmentChanged(0);
+            }
+        });
+        return menuview;
+    }
+}
