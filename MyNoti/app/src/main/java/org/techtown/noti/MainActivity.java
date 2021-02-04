df477f0c23974132d1b84d1c0d17134e5311dfd8
Choi-Ji-Hyeok <boss3164@changwon.ac.kr>
2021-02-04 18:55:28 +0900

    안드로이드 앱 프로그래밍 챕터 2-15 notification

---
+++ MyNoti/app/src/main/java/org/techtown/noti/MainActivity.java
@@ -0,0 +1,63 @@
+package org.techtown.noti;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showNoti1();
+            }
+        });
+    }
+
+    private static String CHANNEL_ID = "channel1";
+    private static String CHANNEL_NAME = "channel1";
+
+
+    public void showNoti1(){
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        NotificationCompat.Builder builder = null; // 예전 버전 단말까지 기능 지원
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            if (manager.getNotificationChannel(CHANNEL_ID) == null) {
+                manager.createNotificationChannel(new NotificationChannel(
+                        CHANNEL_ID,
+                        CHANNEL_NAME,
+                        NotificationManager.IMPORTANCE_DEFAULT
+                ));
+            }
+
+            builder = new NotificationCompat.Builder(this,CHANNEL_ID);
+        } else {
+            builder = new NotificationCompat.Builder(this);
+        }
+
+        builder.setContentTitle("간단 알림");
+        builder.setContentText("간단 알림 메시지입니다.");
+        builder.setSmallIcon(android.R.drawable.ic_menu_view);
+
+
+        Notification noti = builder.build();
+        manager.notify(1,noti);
+
+    }
+}
